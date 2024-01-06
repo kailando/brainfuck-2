f1cf9342990f7a29b15170cce49d8770f50e56f2
kailando <94944679+kailando@users.noreply.github.com>
2024-01-06 14:22:12 -0600

    Update bf.py

--- bf.py
+++ bf.py
@@ -1,26 +1,27 @@
 from sys import argv
-codeChars, mem_size, mem, pointer, ram, ptr, stack=[">", "<", "-", "+", "^", ".", ",", "[", "]", "#", "!"], 8, [0] * 8, 0, 0, 0, []
-try: code=''.join(char for char in open(argv[1]).read() if char in codeChars)
-except IndexError: code=''.join(char for char in input("Brainf**k V.2 code:\n") if char in codeChars)
+mem_size, mem, pointer, ram, ptr, stack=8, [0]*8, 0, 0, 0, []
+try: code=''.join(char for char in open(argv[1]).read() if char in "><-+^.,[]#!")
+except IndexError: code=''.join(char for char in list(input("Brainf**k V.2 code:\n")) if char in "><-+^.,[]#!")
 while ptr<len(code):
     char, ptr=code[ptr], ptr+1
-    if char==codeChars[0]: pointer+=1
-    elif char==codeChars[1]: pointer-=1
-    elif char==codeChars[2]: ram-=1
-    elif char==codeChars[3]: ram+=1
-    elif char==codeChars[4]: mem[pointer]=ram
-    elif char==codeChars[5]: ram=mem[pointer]
-    elif char==codeChars[6]: print(chr(mem[pointer]), end="")
-    elif char==codeChars[7]:
-        if mem[pointer]==0:
-            stack.append(ptr)
-            while stack:
-                ptr+=(code[ptr]=="[")-(code[ptr]=="]")
-                stack.append(ptr) if code[ptr]=="[" else stack.pop()
-    elif char==codeChars[8]:
-            ((ptr:=stack[-1]) if mem[pointer]!=0 else (stack.pop()))
-        else:
-            stack.pop()
-    elif char==codeChars[9]: ram=min(255, max(0, int(input("0-255: "))))
-    elif char==codeChars[10]: exit()
+    match char:
+        case "><-+^.,[]#!"[0]: pointer+=1
+        case "><-+^.,[]#!"[1]: pointer-=1
+        case "><-+^.,[]#!"[2]: ram-=1
+        case "><-+^.,[]#!"[3]: ram+=1
+        case "><-+^.,[]#!"[4]: mem[pointer]=ram
+        case "><-+^.,[]#!"[5]: ram=mem[pointer]
+        case "><-+^.,[]#!"[6]: print(chr(mem[pointer]), end="")
+        case "><-+^.,[]#!"[7]:
+            if mem[pointer]==0:
+                stack.append(ptr)
+                while stack:
+                    ptr+=(code[ptr]=="[")-(code[ptr]=="]")
+                    stack.append(ptr) if code[ptr]=="[" else stack.pop()
+        case "><-+^.,[]#!"[8]:
+                ((ptr:=stack[-1]) if mem[pointer]!=0 else (stack.pop()))
+            else:
+                stack.pop()
+        case "><-+^.,[]#!"[9]: ram=min(255, max(0, int(input("0-255: "))))
+        case "><-+^.,[]#!"[10]: exit()
     mem_size, pointer, ram, mem, ram=mem_size+pointer>=mem_size, max(pointer, 0), max(0, min(255, ram)), [min(255, m) for m in mem], min(255, max(0, ram))
