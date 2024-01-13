6f86b908be162bea2beb7a97302f18a3c7b642bf
kailando <94944679+kailando@users.noreply.github.com>
2024-01-13 09:34:47 -0600

    Shortened bf.py

--- bf.py
+++ bf.py
@@ -5,24 +5,24 @@ except IndexError: code=''.join(char for char in list(input("Brainf**k V.2 code:
 while ptr<len(code):
     char, ptr=code[ptr], ptr+1
     match char:
-        case "><-+^.,[]#!*"[0]: pointer+=1
-        case "><-+^.,[]#!*"[1]: pointer-=1
-        case "><-+^.,[]#!*"[2]: ram-=1
-        case "><-+^.,[]#!*"[3]: ram+=1
-        case "><-+^.,[]#!*"[4]: mem[pointer]=ram
-        case "><-+^.,[]#!*"[5]: ram=mem[pointer]
-        case "><-+^.,[]#!*"[6]: print(chr(mem[pointer]), end="")
-        case "><-+^.,[]#!*"[7]:
+        case ">": pointer+=1
+        case "<": pointer-=1
+        case "-": ram-=1
+        case "+": ram+=1
+        case "^": mem[pointer]=ram
+        case ".": ram=mem[pointer]
+        case ",": print(chr(mem[pointer]), end="")
+        case "[":
             if mem[pointer]==0:
                 stack.append(ptr)
                 while stack:
                     ptr+=(code[ptr]=="[")-(code[ptr]=="]")
                     stack.append(ptr) if code[ptr]=="[" else stack.pop()
-        case "><-+^.,[]#!*"[8]:
+        case "]":
                 ((ptr:=stack[-1]) if mem[pointer]!=0 else (stack.pop()))
             else:
                 stack.pop()
-        case "><-+^.,[]#!*"[9]: ram=min(255, max(0, int(input("0-255: "))))
-        case "><-+^.,[]#!*"[10]: exit()
-        case "><-+^.,[]#!*"[11]: print("", end="")
+        case "#": ram=min(255, max(0, int(input("0-255: "))))
+        case "!": exit()
+        case "*": print("", end="")
     mem_size, pointer, ram, mem, ram=mem_size+pointer>=mem_size, max(pointer, 0), max(0, min(255, ram)), [min(255, m) for m in mem], min(255, max(0, ram))
